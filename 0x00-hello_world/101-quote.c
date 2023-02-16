b82688fd60955d134c5467f52504c0a397f80caf
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-16 10:49:00 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a program that print a line to standard erro
+ * Return: 1 (success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
