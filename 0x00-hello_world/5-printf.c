4741e0be78f19c561ac8f99713b3354e57ad5cab
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-16 06:40:20 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - With proper ,but the
+*out come is a piece of art
+*Return: 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
