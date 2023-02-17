649c8bf7ce3804d55b9e25edfad290ee3c9c0207
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-17 05:01:22 -0800

    alphas

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0) (success)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while
+(c <= 'z')
+{
+	putchar(c);
+	c++;
+}
+putchar ('\n');
+return (0);
+}
