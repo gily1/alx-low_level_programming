eab23416ee6756e60c1cf67d4a4a4ba54da7448b
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-20 03:53:04 -0800

    alphas revs

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - program for revers alphas
+ * Return: 0 alwys
+ */
+int main(void)
+{
+char a = 'z';
+while (a < 'z')
+{
+putchar(a);
+a--;
+}
+putchar('\n');
+return (0);
+}
