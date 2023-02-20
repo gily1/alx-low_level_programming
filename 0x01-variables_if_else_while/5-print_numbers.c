ab1f57b9caa38412a4e0e04b30cec2b9291963f4
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-20 03:15:08 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - program for numbers of base 10 starting
+ * from 0
+ * Return: 0 always
+ */
+int main(void)
+{
+int a = 0;
+for (a = 0 ; a < 10; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
