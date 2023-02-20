e4a87b664feb075753dfda0927df804608cb73ca
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-20 03:35:33 -0800

    numberzz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - program for numberz with putchar function only
+ * Return: 0 alwys
+ */
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
