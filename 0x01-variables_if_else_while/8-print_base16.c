4d136f2d994d9de0d3faedadaff98abc022e794b
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-20 05:50:13 -0800

    hexas

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - program for hexadecimal numbers
+ * Return: 0 alws
+ */
+int main(void)
+{
+int a;
+char b;
+while (a <= 10)
+{
+putchar(a + '0');
+a++;
+}
+while (b <= 'f')
+{
+putchar(b);
+b++;
+}
+putchar('\n');
+return (0);
+}
