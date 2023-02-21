d6e5fa8c68f3caba4083fe39bb302947dfa3f8e2
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-21 13:26:21 -0800

    fibonocci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,7 +7,6 @@ int main(void)
 {
 int i = 0;
 long j = 1, k = 2;
-printf("%ld", j);
 while (i < 50)
 {
 if (i == 0)
@@ -16,7 +15,7 @@ printf(", %ld", j);
 }
 else if (i == 1)
 {
-printf(", %ld",k);
+printf(", %ld", k);
 }
 else
 {
