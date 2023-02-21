67dd2e2e2af42c1c21c4fcf76c62b25918c1fa22
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-21 06:49:36 -0800

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,18 +9,16 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+')
+_putchar(43);
 return (1);
 }
 else if (n < 0)
 {
-putchar('-');
-return ('-1');
+_putchar(45);
+return (-1);
 }
 else
-(n = 0)
 {
-putchar('0');
+_putchar(48);
 return (0);
-}
 }
