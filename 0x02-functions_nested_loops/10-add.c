48076d332700b464c9a70a1c29b978441386c735
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-21 10:21:28 -0800

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * add - adding two numbers
+ * @a: frst number
+ * @b:secnd nbr
+ * Return: 0 alwys
+ */
+int add(int, int)
+{
+int a;
+int b;
+int sum;
+sum = a+ b;
+putchar('the sum is :'" %d",sum)
+return (0);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,9 +6,9 @@
 void times_table(void)
 {
 int x, y, z, u, d;
-for (x = '0'; x <= 9; x++)
+for (x = 0; x <= 9; x++)
 {
-for (y = '0'; y <= 9; y++)
+for (y = 0; y <= 9; y++)
 {
 z = x * y;
 if (z > 9)
@@ -22,7 +22,7 @@ _putchar(u + '0');
 }
 else
 {
-if (y != '0')
+if (y != 0)
 {
 _putchar(44);
 _putchar(32);
