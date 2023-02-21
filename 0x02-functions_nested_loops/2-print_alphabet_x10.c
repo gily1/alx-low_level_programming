42d3c2f8fd4b6b0af2bbd427c65019e243fc295f
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-21 03:38:52 -0800

    10x alphas

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,8 +3,8 @@ include "main.h"
  * print_alphabet_x10 - printing alphabets 10 times
  * Return: 0 always
  */
-void print_alphabte_x10(void)
-{       
+void print_alphabet_x10(void)
+{
 char c = 'a';
 int a = 0;
 for (a = 0; a < 10; a++)
