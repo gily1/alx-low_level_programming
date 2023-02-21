ca2197a524c880cf54814e561f89605411e24428
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-21 03:01:22 -0800

    alphabtes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - program for alpabtes in a function
+ * Return: 0 always
+ */
+void print_alphabet(void);
+int main(void);
+{
+char c = 'z';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
