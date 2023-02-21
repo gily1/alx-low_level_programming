d32cd4a3c591c7572be605c7338b0c37924c1946
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-21 15:10:23 -0800

    natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_to_98 - pritn all natural numbers fro n to 98
+ * @n: starting point
+ * Reeturn: alwys 0
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d,", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+}
