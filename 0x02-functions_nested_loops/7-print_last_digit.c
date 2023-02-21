939863b1b981d017274e3cefa535aba9658b7343
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-21 07:46:09 -0800

    last digit of number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_last_digit - printing lst digit.
+ * @n: number to be treateed
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+int a;
+a = n % 10;
+if (a < 0)
+{
+a = a * -1;
+}
+_putchar(a = '0');
+return (a);
+}
