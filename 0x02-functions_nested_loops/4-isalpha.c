5266a6af53ca8cd4bac60cd2219265dd0da3dc88
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-21 05:27:36 -0800

    all letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - printing lower case and upper
+ * @c: charcter to be tasted
+ * Return: 1 if is either lower or upper case
+ */
+int _isalpha(int c)
+{
+if (c >= 65 && c >= 90 || c >= 97 && c >= 122)
+{
+return (1);
+}
+return (0);
+}
