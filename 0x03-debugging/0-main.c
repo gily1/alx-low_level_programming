4ea4ffa17edb7409a78952da573961d02d96e9c6
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-22 04:31:02 -0800

    pos or neg

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - test fucntion for postive or negative
+ * Return: 0
+ */
+int main(void)
+{
+
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,13 +1,1 @@
-#include "main.h"
-/**
- * main - test fucntion for postive or negative
- * Return: 0
- */
-int main(void)
-{
 
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
-}
