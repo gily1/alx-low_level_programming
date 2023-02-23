32f23c44d33595e039d98f882047ffa966c9cd68
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-23 05:28:17 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,7 +6,7 @@
 void more_numbers(void)
 {
 int x, y;
-for(x = 0; x < 10; x++)
+for (x = 0; x < 10; x++)
 {
 for (y = 0; y <= 14; y++)
 {
@@ -14,7 +14,7 @@ if (y > 9)
 {
 _putchar((y / 10) + '0');
 }
-_putchar((y % 10) + '0'); 
+_putchar((y % 10) + '0');
 }
 _putchar('\n');
 }
