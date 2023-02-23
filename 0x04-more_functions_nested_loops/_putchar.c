32a9e84ac1b3c3bd574e6859fde0a9fc27e39c9e
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-23 02:53:51 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<unstd.h>
+/**
+ * _putchar - writes charchter on screen
+ * @c: the charcter to print
+ *
+ * Return: on success 1
+ * on error, -1 is returned and error is a set set appropriate
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
