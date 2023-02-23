a4eb24f007a4ddd13bbb8ab170bcf80f70a67c29
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-23 09:10:10 -0800

    last last

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * print_number -prints an integer
+ * @n: the integer to print
+ * Return: nothing
+ */
+void print_number(int n)
+{
+unsigned int k = n;
+if (n < 0)
+{
+n *= -1;
+k = n;
+_putchar('-');
+}
+k /= 10;
+if (k != 0)
+print_number(k);
+putchar((unsigned int) n % 10 + '0');
+}
