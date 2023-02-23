f44d88a6846417c29f13b017aa482c0bf8b21bec
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-23 03:04:57 -0800

    digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - check if a number is btn 0 and 9
+ * @c: char to be checked
+ *
+ * Return: 0 and 1
+ */
+int _isdigit(int c)
+{
+if(c >= '0' && c <= '9')	
+return(1);
+else 
+return (0);	
+}
