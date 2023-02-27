522aa0a693688c7f23790daf470276c32d283438
‘gily1’ <niyogilbert2023@gmail.com>
2023-02-27 05:13:04 -0800

    swaping

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,10 +7,7 @@
  */
 void swap_int(int *a, int *b)
 {
-int a = 98;
-int b = 42;
-printf("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-printf("a=%d, b=%d\n", a, b);
-return (0);
+int temp = *a;
+*a = *b;
+*b = temp;
 }
