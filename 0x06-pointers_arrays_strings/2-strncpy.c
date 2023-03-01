ea8ff2cf33ac2b10f16bd5e2b37e837618f9d6aa
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-01 04:49:23 -0800

    char *_strncpy(char *dest, char *src, int n

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * _strncpy - fucntion of copying codes
+ * @dest:first string
+ * @src:second string
+ * @n:number
+ * Return: strings.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+while (src[i++])
+j++
+for (i = 0; src[i] && i < n; i++)
+dest[i] = src[i];
+for (i = j; i < n; i++)
+dest[i] = '\0';
+return (dest);
+}
