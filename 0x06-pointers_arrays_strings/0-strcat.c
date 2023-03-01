2d5136e3d7e63dfb689aa9e6ff336a3db7fda3cd
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-01 03:02:39 -0800

    concatenating

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * _strcat - function to hold strings to be conancatenated
+ * @src: the second string
+ *@dest: first string
+ * Return: string.
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+char s1[20];
+while (dest[i] != '\0')
+{
+s1[j] = dest[i];
+i++;
+j++;
+}
+i = 0;
+while (src[i] != '\0')
+{
+s1[j] = src[i];
+i++;
+j++;
+}
+s1[j] = '\0';
+printf("concatenation is :%s", s1);
+}
