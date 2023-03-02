98758dfb38d7d93fdf224c7603fe12f322cc8a38
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-02 04:36:56 -0800

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,17 +3,19 @@
 int main(void)
 {
 int n;
-int s;
-int a[s];
+int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
-/**
- * main - body of codes for magic
- * p modification not allowed
- * Return: 0
- */
-*(p + s) = 98;
+/*
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+  */
+*(p + 5) = 98;
 /*so that prints 98\n*/
 printf("a[2] = %d\n", a[2]);
 return (0);
