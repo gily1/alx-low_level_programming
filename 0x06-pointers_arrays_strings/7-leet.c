6017255200d36e7275724a6168419930e722cebc
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-02 06:14:32 -0800

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
  *leet - encode codes for the funtion
- *@n: input value 
+ *@n: input value
  *Return: value
  */
 char *leet(char *n)
@@ -11,7 +11,7 @@ char s1[] = "aAeEoOtTlL";
 char s2[] = "4433007711";
 for (i = 0; n[i] != '\0'; i++)
 {
-for (j = 0; j <10; j++)
+for (j = 0; j < 10; j++)
 {
 if (n[i] == s1[j])
 {
