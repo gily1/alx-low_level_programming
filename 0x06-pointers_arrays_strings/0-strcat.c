cc415d4fdc1961975527a669e5499af29a9373a9
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-02 00:58:44 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,9 +7,8 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i = 0;
+int i;
 int j = 0;
-char s1[20];
 while (dest[i])
 {
 i++;
@@ -19,6 +18,6 @@ for (j = 0; src[j] != 0; j++)
 dest[i] = src[j];
 i++;
 }
-dest[j] = '\0';
+dest[i] = '\0';
 return (dest);
 }
