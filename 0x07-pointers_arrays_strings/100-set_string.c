b55762240cd6851562c58ba4b9287f2506d64bc0
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-06 01:52:40 -0800

    changing pointer pointer to string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#inlcude "main.h"
+/**
+ * set_string - function to hold poinetrs
+ * @s: frst string
+ * @to:char to hold string
+ * Return:something
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
