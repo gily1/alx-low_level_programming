c49a71610f7bd4392a2b21d50e07147907fa87c1
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-06 02:27:01 -0800

    memecopy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - head fucnton
+* @dest: storage of memory
+ *@src: memory copy location
+* @n: number of bytes
+ *Return: something
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+int r;
+int i;
+for (r = 0; r < i; r++)
+{
+dest[r] = src[r];
+n--;
+}
+return (dest);
+}
