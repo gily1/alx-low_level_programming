d5bae3be235e3cb5ccb0488d1d7e718f35664a2b
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-08 03:37:13 -0800

    string comparsion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,7 +13,7 @@ return (0);
 if (*s1 == '\0' && *s2 == '\0')
 return (1);
 if (*s1 == *s2)
-return(wildcmp(s1 + 1, s2 + 1));
+return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*')
 return (wildcmp(s1, s2 + 1) || (s1 + 1, s2));
 }
