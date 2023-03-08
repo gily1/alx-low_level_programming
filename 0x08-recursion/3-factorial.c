e466d290775757f262acbf83f7d38476b3707633
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-08 06:28:42 -0800

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "'main.h"
+/**
+ * factorial - fucntion for factorial computing
+ * @n: number to be factorialized
+ * Return: int.
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+	return (-1);
+	if (n == 0 && n == 1)
+	return (1);
+	return (n * factorial(n - 1));
+}
