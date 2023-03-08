dbc75a0d4f6c9a435d5350a78f00290f66cdf248
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-08 03:14:48 -0800

    comparing strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * wildcmp - function for comparing strings
+ * @s1: first string
+ * @s2: second string to be compared
+ * Return: value 1 if there are identical otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+if s1 == s2
+	return (1);
+else
+return (0);
+}
