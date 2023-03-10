f98b6f0fc6e71e96acbd73e4819590f88fd24093
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-10 05:37:15 -0800

    counting the arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - function for counting nbr of arguments
+ *@argc: argument counter
+ @argv: argument vector
+ *Return: o broh
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+    printf("%d\n", argc - 1);
+    return (0);
+}
+
