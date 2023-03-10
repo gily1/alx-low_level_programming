27a871c45655f0f151513ccef7bcc16e60b60c76
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-10 06:48:55 -0800

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include"main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main- adding numbers
+ * @argv:vector
+ * @argc:arguments conter
+ * Return: product.
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int j;
+int mult = 1;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+mult *= atoi(argv[i]);
+}
+printf("%d\n", mult);
+return (0);
+}
+
