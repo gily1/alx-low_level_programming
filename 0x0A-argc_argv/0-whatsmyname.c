73208fa28b9fe5798d896f27d61b2d8ce9705947
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-10 05:11:53 -0800

    program that writes its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,12 @@
  * main - argumental fucntion
  *@argc :argument counter
  *@argv:argument vectore
+ *Return : 0 succes
  */
 int main(int argc, char *argv[])
 {
 (void) argc;
 printf("%s\n", argv[0]);
-return 0;
+return (0);
 }
 
