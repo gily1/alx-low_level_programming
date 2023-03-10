f3f687dd7c63a890f2d08b9c2dc535537f175545
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-10 05:10:16 -0800

    program that writes its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * main - argumental fucntion
+ *@argc :argument counter
+ *@argv:argument vectore
+ */
 int main(int argc, char *argv[])
-{	(void) argc;
-    printf("%s\n", argv[0]);
-    return 0;
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return 0;
 }
 
