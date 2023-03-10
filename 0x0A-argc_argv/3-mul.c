ac56a5121604b4261158b71b63d60f1f4abf4ae0
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-10 07:18:50 -0800

    multplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,36 +1,28 @@
-#include"main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include"main.h"
 /**
- * main- adding numbers
- * @argv:vector
- * @argc:arguments conter
- * Return: sum
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
 {
-int i;
-int j;
-int mult = 1;
-if (argc == 1)
-{
-printf("Error\n");
-return (0);
-}
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (!isdigit(argv[i][j]))
+int num1;
+int num2;
+int result;
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-}
-mult *= atoi(argv[i]);
-}
-printf("%d\n", mult);
-return (0);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
+printf("%d\n", result);
+return 0;
 }
 
