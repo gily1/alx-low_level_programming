2ed3dee9b87b3e7c975e41735ce73af3ce1ab6e3
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-13 06:44:19 -0700

    spliting string

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,7 +19,7 @@ for (i = 0; i < ac; i++)
 {
 total_len += strlen(av[i]) + 1;
 }
-str = (char *) malloc(sizeof(char) * total_len);
+str = (char *) malloc(sizeof(char) * total_len + 1);
 if (str == NULL)
 {
 return (NULL);
