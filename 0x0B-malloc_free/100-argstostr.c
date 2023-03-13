af04d06a1f8fa259365d2f1eed1633de5dab1a01
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-13 06:29:58 -0700

    spliting string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - function for concatanating strings
+ *@ac: string
+ @av: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+int i;
+char *str;
+size_t len, pos, total_len;
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+total_len = 0;
+for (i = 0; i < ac; i++)
+{
+total_len += strlen(av[i]) + 1;
+}
+str = (char *) malloc(sizeof(char) * total_len);
+if (str == NULL)
+{
+return (NULL);
+}
+pos = 0;
+for (i = 0; i < ac; i++)
+{
+len = strlen(av[i]);
+memcpy(str + pos, av[i], len);
+pos += len;
+str[pos++] = '\n';
+}
+str[pos] = '\0';
+return (str);
+}
+
