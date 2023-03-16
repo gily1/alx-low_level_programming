b31499e9d988738d073f2b2b97d1d65a6e3d4afd
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-16 07:40:43 -0700

    h

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,13 +8,11 @@
  */
 void *malloc_checked(unsigned int b)
 {
-  void *ptr = malloc(b);
-  
-  if (ptr == NULL)
-  {
-    exit(98);
-  }
-
-  return(ptr);
+void *ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return(ptr);
 }
 
