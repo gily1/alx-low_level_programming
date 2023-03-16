1d74ce99c6231db21e07cb6d848f3b77494a8dad
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-16 08:17:51 -0700

    multplication

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,41 +1,90 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#define ERR_MSG "Error"
 /**
- * is_positive_integer - function
- * @n: charcter
- * Return:results
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int is_positive_integer(char *n)
+int is_digit(char *s)
 {
-	int i;
-for (i = 0; n[i] != '\0'; i++)
+int i = 0;
+while (s[i])
 {
-if (n[i] < '0' || n[i] > '9')
-{
+if (s[i] < '0' || s[i] > '9')
 return (0);
+i++;
 }
+return (1);
 }
-return (atoi(n) > 0);
-}
-int multiply(char *num1, char *num2)
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+int _strlen(char *s)
 {
-return (atoi(num1) * atoi(num2));
-}
-
-int main(int argc, char **argv)
+int i = 0;
+while (s[i] != '\0')
 {
-int result;
-unsigned int invalid_argc = (argc != 3);
-unsigned int invalid_arg1 = !is_positive_integer(argv[1]);
-unsigned int  invalid_arg2 = !is_positive_integer(argv[2]);
-if (invalid_argc || invalid_arg1 || invalid_arg2)
+i++;
+}
+return (i);
+}
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
 {
 printf("Error\n");
-return (98);
+exit(98);
 }
-result = multiply(argv[1], argv[2]);
-printf("%d\n", result);
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+char *s1, *s2;
+int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+s1 = argv[1], s2 = argv[2];
+if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+errors();
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+len = len1 + len2 + 1;
+result = malloc(sizeof(int) * len);
+if (!result)
+return (1);
+for (i = 0; i <= len1 + len2; i++)
+result[i] = 0;
+for (len1 = len1 - 1; len1 >= 0; len1--)
+{
+digit1 = s1[len1] - '0';
+carry = 0;
+for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+{
+digit2 = s2[len2] - '0';
+carry += result[len1 + len2 + 1] + (digit1 *digit2);
+result[len1 + len2 + 1] = carry % 10;
+carry /= 10;
+}
+if (carry > 0)
+result[len1 + len2 + 1] += carry;
+}
+for (i = 0; i < len - 1; i++)
+{
+if (result[i])
+a = 1;
+if (a)
+putchar(result[i] + '0');
+}
+if (!a)
+putchar('0');
+putchar('\n');
+free(result);
 return (0);
 }
-
