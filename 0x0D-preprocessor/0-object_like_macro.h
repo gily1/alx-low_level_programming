b2b165b92bd60fcc831e22b793bf744fe05ca0fc
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-20 04:32:55 -0700

    creating buffer named size with token 1024

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,5 @@
 #include <stdlib.h>
-#include <sstdio.h>
+#include <stdio.h>
+
 #define SIZE 1024
-/**
- * main - main function
- * Return: 0 succesful
- */
-int main(void)
-{
-printf ('\n');
-return (0);
-}
+
