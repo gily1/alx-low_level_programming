a1803a76da21b18bf3d3503b913a35f9e0d737d9
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-22 05:40:34 -0700

    updating my function_pointer file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
+	#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 
 void array_iterator(int *array, size_t size, void (*action)(int));
-
-int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
