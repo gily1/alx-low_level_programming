cbcb22d4e0d6a5a96858c3cd1f538f03faddaaec
‘gily1’ <niyogilbert2023@gmail.com>
2023-03-22 04:06:18 -0700

    creating function that print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+	#include <stdio.h>
+/**
+ * print_name - function to hold codes for printing names
+ * @name: name to be printed
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
+
